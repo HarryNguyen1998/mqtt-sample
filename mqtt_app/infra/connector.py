22e85cc9a0c4d98504b0328fb0ce492b85ccc7eb
Quan Nguyen <harrynguyen719@gmail.com>
2023-06-23 16:59:00 +0300

    feat: update logger for child class

--- mqtt_app/infra/connector.py
+++ mqtt_app/infra/connector.py
@@ -5,8 +5,6 @@ import threading
 
 import paho.mqtt.client as mqttc
 
-logger = logging.getLogger(__name__)
-
 
 class MQTTConnector(abc.ABC):
     """A connector that handles connection to a MQTT broker."""
@@ -16,23 +14,24 @@ class MQTTConnector(abc.ABC):
         self._connected = False
         self._bad_connect = False
         self._disconnect_flag = threading.Event()
+        self.log = logging.getLogger(self.__class__.__name__)
 
         self.client = mqttc.Client(name)
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
 
         if os.environ.get("APP_DEBUG"):
-            self.client.enable_logger(logger)
+            self.client.enable_logger(self.log)
 
     def connect(self):
         """Set up connection to the MQTT broker."""
-        logger.info(f"Connecting to {self._broker_addr}...")
+        self.log.info(f"Connecting to {self._broker_addr}...")
         self.client.connect(self._broker_addr)
         self.client.loop_start()
 
     def disconnect(self):
         """Closes connection to the MQTT broker."""
-        logger.info(f"Disconnecting from {self._broker_addr}...")
+        self.log.info(f"Disconnecting from {self._broker_addr}...")
 
         self.client.disconnect()
         self.client.loop_stop()
@@ -56,16 +55,16 @@ class MQTTConnector(abc.ABC):
     def on_connect(self, client, userdata, flags, conn_rc):
         if conn_rc == 0:
             self._connected = True
-            logger.info("MQTT connection established")
+            self.log.info("MQTT connection established")
         else:
             self._bad_connect = True
-            logger.error(f"MQTT connection failed, rc={conn_rc}")
+            self.log.error(f"MQTT connection failed, rc={conn_rc}")
 
     def on_disconnect(self, client, userdata, conn_rc):
         if conn_rc == 0:
             self._disconnect_flag.set()
-            logger.info("Disconnect successfully")
+            self.log.info("Disconnect successfully")
         else:
-            logger.error(f"Unexpected disconnect, rc={conn_rc}")
+            self.log.error(f"Unexpected disconnect, rc={conn_rc}")
 
         self._connected = False
--- mqtt_app/infra/publisher.py
+++ mqtt_app/infra/publisher.py
@@ -1,8 +1,4 @@
-import logging
-
 from infra.connector import MQTTConnector
-
-logger = logging.getLogger(__name__)
 
 
 class Publisher(MQTTConnector):
