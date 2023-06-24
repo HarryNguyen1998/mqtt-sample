f307c484ac3afc4be4ddf0929202b847d6bf34e5
Quan Nguyen <harrynguyen719@gmail.com>
2023-06-24 20:06:22 +0300

    fix: mismatch env var

--- mqtt_app/mqtt_app/config.py
+++ mqtt_app/mqtt_app/config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     DB_URL = os.getenv("DB_URL", "mysql://root:root@localhost:3306/testing")
     BROKER_ADDR = os.getenv("BROKER_ADDR", "mqtt_broker")
-    DEBUG_PRINT = os.getenv("APP_DEBUG", False)
+    DEBUG_PRINT = os.getenv("DEBUG_PRINT", False)
