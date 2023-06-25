94be6f5f3a3d6db14e084c0d94d534fcf3f01e2d
Quan Nguyen <harrynguyen719@gmail.com>
2023-06-25 22:56:28 +0300

    feat: remove route for individual session

--- api/api/db/repository.py
+++ api/api/db/repository.py
@@ -18,18 +18,3 @@ class ChargerSessionRepository:
         db_models = db_models.scalars().all()
         models = [ChargerSessionModel.from_orm(db_model) for db_model in db_models]
         return models
-
-    async def get(self, session_id: int) -> ChargerSessionModel | None:
-        """Get a charger session given the specified id.
-
-        :param session_id: the session id to query against.
-        :return: a charger session model, or None if not found.
-        """
-        query = select(ChargerSessionOrm).where(
-            ChargerSessionOrm.session_id == session_id
-        )
-        db_model = await self._session.execute(query)
-        db_model = db_model.scalars().first()
-        if not db_model:
-            return db_model
-        return ChargerSessionModel.from_orm(db_model)
--- api/api/routes/charger_sessions.py
+++ api/api/routes/charger_sessions.py
@@ -18,22 +18,3 @@ async def get_all() -> list[ChargerSessionModel]:
         charger_sessions = await repo.get_all()
 
     return charger_sessions
-
-
-@router.get("/{id}")
-async def get(id: int) -> ChargerSessionModel:
-    """Get a charger session by ID.
-
-    :param id: ID of the charger session to get.
-    :return: a charger session.
-    :raise: if no charger session with the specified id is found, a 404 error is
-    raised. 
-    """
-    charger_session = None
-    async with get_session() as session:
-        repo = ChargerSessionRepository(session)
-        charger_session = await repo.get(id)
-
-    if not charger_session:
-        raise HTTPException(status_code=404, detail="Charger session not found.")
-    return charger_session
